b1e1ded51240b654d0dfe67212aee452040914fb
Richard <richard.hape@devacademy.co.nz>
2024-03-15 14:54:07 +1300

    make change to App.tsx

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,29 +1,8 @@
-import { useState } from 'react'
-import { getGreeting } from '../apiClient.ts'
-import { useQuery } from '@tanstack/react-query'
-
-const App = () => {
-  const [count, setCount] = useState(0)
-
-  const {
-    data: greeting,
-    isError,
-    isPending,
-  } = useQuery({ queryKey: ['greeting', count], queryFn: getGreeting })
-
-  if (isPending) return <p>Loading...</p>
-
+function App() {
   return (
-    <>
-      {count}
-      <h1 className="text-3xl font-bold underline">{greeting}</h1>
-      {isError && (
-        <p style={{ color: 'red' }}>
-          There was an error retrieving the greeting.
-        </p>
-      )}
-      <button onClick={() => setCount(count + 1)}>Click</button>
-    </>
+    <div>
+      <h1>HWelcome to the Human Skills Reflection App</h1>
+    </div>
   )
 }
 
