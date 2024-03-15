e574fcad1f8c61ca907b2f9b13a6bee93f983544
Richard <richard.hape@devacademy.co.nz>
2024-03-15 15:54:25 +1300

    create Logo.tsx

--- client/components/AppLayout.tsx
+++ client/components/AppLayout.tsx
@@ -1,13 +1,14 @@
 import { Outlet } from 'react-router-dom'
 import Home from "./Home"
 import Footer from './Footer'
+import Logo from './Logo'
 
 function App() {
   return (
     <div className="min-h-screen" style={{ backgroundColor: '#40276E' }}>
       <Home />
-      {/* <Logo className="pt-7" />
-      <div className="flex justify-center items-center mt-4">
+      <Logo className="pt-7" />
+      {/* <div className="flex justify-center items-center mt-4">
         <Login className="mr-5" />
         <Register />
       </div> */}
---
+++ client/components/Logo.tsx
@@ -0,0 +1,20 @@
+interface LogoProps {
+  className?: string
+}
+
+function Logo({ className }: LogoProps) {
+  return (
+    <>
+      <div className={`flex items-center justify-center p-5 ${className}`}>
+        <img
+          src="./images/logo.png"
+          alt="Dev Academy Logo"
+          style={{ width: '20%' }}
+        ></img>
+      </div>
+    </>
+  )
+
+}
+
+export default Logo
