9164cf92ce96b3832409e52f7862c59689a9bb6e
Richard <richard.hape@devacademy.co.nz>
2024-05-09 16:27:55 +1200

    made small change to component names

--- client/router.tsx
+++ client/router.tsx
@@ -1,5 +1,5 @@
 import { Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
-import App from "./components/AppLayout";
+import App from "./components/App";
 import About from "./components/About";
 
 const router = createBrowserRouter(
