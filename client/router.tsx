9d35892abb60db4a029da3789f6c817cfb07d74e
Richard <richard.hape@devacademy.co.nz>
2024-05-09 15:58:32 +1200

    created router.tsx now working on routes

---
+++ client/router.tsx
@@ -0,0 +1,13 @@
+import { Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import App from "./components/AppLayout";
+import About from "./components/About";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index element={<About />} />
+    </Route>
+  )
+)
+
+export default router
