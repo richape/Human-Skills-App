974547a09c9afee9a355b44ae83921548837ab81
Richard <richard.hape@devacademy.co.nz>
2024-05-10 13:30:06 +1200

    create a home button component to use on certain pages

--- client/components/About.tsx
+++ client/components/About.tsx
@@ -1,28 +1,38 @@
+import { Link } from "react-router-dom"
+
 function About() {
   return (
-    <div className="text-center">
-      <h1 className="text-yellow-500 text-3xl font-bold mt-4">
-        Welcome to Bootcamp
-      </h1>
-
-      <p className="text-yellow-500">
-        This is your friendly Human Skills reflections App, it has been created
-        to make capturing your reflections during Bootcamp more easily to help
-        complete your Human Skills Trellos (Trellos will be introduced to you
-        during Week 1).
-      </p>
-
-      <p className="text-yellow-500">
-        Please be sure to read each question thoroughly. If you have any
-        questions please do not hesitate to contact your Human Skills
-        Facilitator.
-      </p>
-
-      <p className="text-yellow-500">
-        If you do submit anything into your Trello cards for your facilitator to
-        mark, please tag them as well.
-      </p>
-    </div>
+    <>
+      <div className="flex items-center justify-center">
+        <Link to='/' className="p-10">
+          <img src="/images/logo.png" alt="Dev Academy Logo" style={{ width: '10%' }} />
+        </Link>
+        <div className="flex text-center">
+          <h1 className="text-yellow-500 text-5xl font-bold font-sans">
+            Welcome to Bootcamp
+          </h1>
+        </div>
+      </div>
+        <div>
+          <div className="text-yellow-500 p-10 text-3xl font-sans">
+            <p className="p-6 px-10">
+            This is your friendly Human Skills reflections App, it has been created
+            to make capturing your reflections during Bootcamp more easily to help
+            complete your Human Skills Trellos (Trellos will be introduced to you
+            during Week 1).
+            </p>
+            <p className="p-6 px-10">
+            Please be sure to read each question thoroughly. If you have any
+            questions please do not hesitate to contact your Human Skills
+            Facilitator.
+            </p>
+            <p className="p-6 px-10">
+            If you do submit anything into your Trello cards for your facilitator to
+            mark, please tag them as well.
+            </p>
+          </div>
+        </div>
+    </>
   )
 }
 
--- client/index.tsx
+++ client/index.tsx
@@ -2,7 +2,6 @@ import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-import App from './components/App.tsx'
 import { RouterProvider } from 'react-router-dom'
 import router from './router.tsx'
 
