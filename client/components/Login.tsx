94aca49f7e3a8af38788a850a71c93dd353f654e
Richard <richard.hape@devacademy.co.nz>
2024-05-14 16:04:01 +1200

    first commit for the day

--- client/components/Login.tsx
+++ client/components/Login.tsx
@@ -7,7 +7,6 @@ function Login({ className }: LoginProps) {
     <div className={className}>
       <div
         className="rounded-md drop-shadow-xl"
-        // p-3.5
         style={{ backgroundColor: '#7E51B2' }}
       >
         <button className="text-yellow-500 py-2 px-5 rounded-lg text-2xl font-bold bg-opacity-75 cursor-pointer">
